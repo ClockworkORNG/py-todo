916eb957741953f184d66f8602ebfb9e9166692c
ilya <illyakobylatskyi@gmail.com>
2020-10-26 20:48:11 +0200

    sqlalchemy removed; pymysql used only

--- app.py
+++ app.py
@@ -1,87 +1,139 @@
 from flask import Flask, render_template, request, redirect
-import json
+import pymysql.cursors
+
+from datetime import datetime
 
 app = Flask(__name__)
-
-# tasks = [
-#  { "id" : 1, "name" : 'Watch TV'},
-#  { "id" : 2, "name" : 'buy eggs'}
-
-# ]
-
-try:
-    tasksFile = open('./tasks.json', 'r')
-    tasks = json.load(tasksFile)
-    tasksFile.close()
-except:
-    tasks = []
-
-
-def writeTasks():
-    tasksFile = open('./tasks.json', 'w')
-    json.dump(tasks, tasksFile, indent=4)
-    tasksFile.close()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', tasks=tasks)
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='1234',
+                                 db='todolist',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    cursor = connection.cursor()
+
+    order = request.args.get('sort')
+    if order is not None:
+        if order == 'asc':
+            sql = "select * from tasks order by expiredDate"
+        elif order == 'desc':
+            sql = "select * from tasks order by expiredDate desc"
+        else:
+            sql = "select * from tasks"
+    else:
+        sql = "select * from tasks"
+    cursor.execute(sql, ())
+    tasks = cursor.fetchall()
+    print(tasks)
+    modTasks = []
+    for task in tasks:
+        fTime = task.get('expiredDate')
+        nTime = datetime.now()
+        if nTime > fTime:
+            marked = False
+        else:
+            marked = True
+        modTasks.append({
+            "task": task,
+            "marked": marked
+        })
+    connection.close()
+    return render_template('index.html', tasks=modTasks)
 
 
 @app.route('/add-task', methods=['POST'])
 def addTask():
     taskName = request.form.get('new-task')
+    expDate = request.form.get('exp-date')
+    expTime = request.form.get('exp-time')
+    print(expDate, expTime)
 
-    m = 0
-    for task in tasks:
-        if task.get('id') > m:
-            m = task.get('id')
+    fullDateTime = f'{expDate} {expTime}:00'
+    # print(fullDateTime)
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='1234',
+                                 db='todolist',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
 
-    task = {
-        'id': m + 1,
-        "name": taskName
-
-    }
-    tasks.append(task)
-
-    writeTasks()
+    cursor = connection.cursor()
+    sql = "insert into tasks(content, creationDate, expiredDate) values(%s, %s, %s)"
+    cursor.execute(sql, (taskName, str(datetime.now()), fullDateTime))
+    connection.commit()
+    connection.close()
     return redirect('/')
 
 
 @app.route('/remove-task', methods=['GET'])
 def removeTask():
     taskId = int(request.args.get('id'))
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='1234',
+                                 db='todolist',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
 
-    for task in tasks:
-        print(task)
-        if task.get('id') == taskId:
-            tasks.remove(task)
-
-    writeTasks()
+    cursor = connection.cursor()
+    sql = "delete from tasks where id=%s"
+    cursor.execute(sql, (taskId))
+    connection.commit()
+    connection.close()
     return redirect('/')
 
 
 @app.route('/edit-task', methods=['GET'])
 def editTask():
     taskId = int(request.args.get('id'))
-    name = ''
-    for task in tasks:
-        if task.get('id') == taskId:
-            name = task.get('name')
 
-    return render_template('item-edit.html', taskValue=name, id=taskId)
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='1234',
+                                 db='todolist',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    cursor = connection.cursor()
+    sql = "select * from tasks where id=%s"
+    cursor.execute(sql, (taskId))
+    task = cursor.fetchall()
+    print(task)
+
+    name = {
+        'content': task[0].get('content'),
+        'expiredDate': datetime.date(task[0].get('expiredDate')),
+        'expiredTime': datetime.time(task[0].get('expiredDate'))
+
+    }
+    connection.close()
+    return render_template('item-edit.html', task=name, id=taskId)
 
 
 @app.route('/save-task', methods=['POST'])
 def saveTask():
     taskName = request.form.get('edit-task')
+
     taskId = int(request.form.get('id'))
+    expDate = request.form.get('exp-date')
+    expTime = request.form.get('exp-time')
+    connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='1234',
+                                 db='todolist',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
 
-    for task in tasks:
-        if task.get('id') == taskId:
-            task['name'] = taskName
-
-    writeTasks()
+    cursor = connection.cursor()
+    sql = "update tasks set content=%s, expiredDate=%s where id=%s"
+    cursor.execute(sql, (taskName, f'{expDate} {expTime}', taskId))
+    connection.commit()
+    connection.close()
     return redirect('/')
 
 
